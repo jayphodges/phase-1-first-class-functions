a16d08bad64afdfd8c091d8a53176d649be782f4
Jay P Hodges <jayphodges@gmail.com>
2022-10-18 18:00:03 -0600

    commit tree 930c0ab44631cabfa50e7cd5e857a147de0d4f64

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback) {
+    return callback()
+}
+
+function returnsANamedFunction() {
+    function aNamedFunction(){}
+    return aNamedFunction
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {}
+}
